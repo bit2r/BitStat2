e89ca1bc4f1d4218ac65c18afe7a1a520c572fc1
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2024-05-16 09:03:18 +0900

    added correlation and simple linear regression

--- 06_reg/simple/shiny/app.R
+++ 06_reg/simple/shiny/app.R
@@ -1,58 +1,77 @@
 library(shiny)
+library(showtext)
+showtext_auto()
 
 ui <- fluidPage(
-  titlePanel("Correlation and Simple Linear Regression"),
+  titlePanel("상관분석과 단순 회귀분석"),
 
   sidebarLayout(
     sidebarPanel(
-      fileInput("file", "Choose CSV File",
-                accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-      tags$br(),
-      checkboxInput("header", "Header", TRUE),
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",", Semicolon = ";", Tab = "\t"),
-                   selected = ","),
-      selectInput("x_var", "X Variable:", ""),
-      selectInput("y_var", "Y Variable:", "")
+      fileInput("file", "CSV 파일 업로드", accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+      radioButtons("dataset", "데이터셋 선택:",
+                   choices = c("mtcars", "USArrests", "업로드 데이터셋"),
+                   selected = "mtcars"),
+      selectInput("y_var", "Y 변수 (종속변수):", ""),
+      selectInput("x_var", "X 변수 (독립변수):", "")
     ),
 
     mainPanel(
-      plotOutput("scatterplot"),
-      verbatimTextOutput("correlation"),
-      verbatimTextOutput("regression")
+      tabsetPanel(
+        tabPanel("산점도", plotOutput("scatterplot")),
+        tabPanel("상관계수",
+                 verbatimTextOutput("correlation"),
+                 verbatimTextOutput("cor_test")),
+        tabPanel("회귀분석", verbatimTextOutput("regression"))
+      )
     )
   )
 )
 
 server <- function(input, output, session) {
 
-  df <- reactive({
-    req(input$file)
-    read.csv(input$file$datapath, header = input$header, sep = input$sep)
+  data <- reactive({
+    if (input$dataset == "업로드 데이터셋") {
+      req(input$file)
+      read.csv(input$file$datapath, header = TRUE)
+    } else {
+      switch(input$dataset,
+             "mtcars" = mtcars,
+             "USArrests" = USArrests)
+    }
   })
 
   observe({
-    updateSelectInput(session, "x_var", choices = names(df()))
-    updateSelectInput(session, "y_var", choices = names(df()))
+    choices <- names(data())[sapply(data(), is.numeric)]
+    updateSelectInput(session, "y_var", choices = choices)
+    updateSelectInput(session, "x_var", choices = choices)
   })
 
   output$scatterplot <- renderPlot({
-    req(input$x_var, input$y_var)
-    plot(df()[[input$x_var]], df()[[input$y_var]],
+    req(input$y_var, input$x_var)
+    plot(data()[[input$x_var]], data()[[input$y_var]],
          xlab = input$x_var, ylab = input$y_var,
-         main = "Scatter Plot")
-    abline(lm(df()[[input$y_var]] ~ df()[[input$x_var]]), col = "red")
+         main = "회귀선이 포함된 산점도")
+    abline(lm(data()[[input$y_var]] ~ data()[[input$x_var]]), col = "red", lwd = 2)
+    legend("topleft", legend = "회귀선", col = "red", lwd = 2, bty = "n")
   })
 
   output$correlation <- renderPrint({
-    req(input$x_var, input$y_var)
-    cor_val <- cor(df()[[input$x_var]], df()[[input$y_var]])
-    paste("Correlation:", round(cor_val, 3))
+    req(input$y_var, input$x_var)
+    cor_val <- cor(data()[[input$x_var]], data()[[input$y_var]])
+    det_coef <- cor_val^2
+    cat("상관계수:", round(cor_val, 3), "\n")
+    cat("결정계수 (R-squared):", round(det_coef, 3))
+  })
+
+  output$cor_test <- renderPrint({
+    req(input$y_var, input$x_var)
+    cor.test(data()[[input$x_var]], data()[[input$y_var]])
   })
 
   output$regression <- renderPrint({
-    req(input$x_var, input$y_var)
-    lm_model <- lm(df()[[input$y_var]] ~ df()[[input$x_var]])
+    req(input$y_var, input$x_var)
+    lm_model <- lm(data()[[input$y_var]] ~ data()[[input$x_var]])
+    names(lm_model$coefficients) <- c("절편", input$x_var)
     summary(lm_model)
   })
 
